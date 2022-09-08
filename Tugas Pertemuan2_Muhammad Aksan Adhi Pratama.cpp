3838a9dbc72e75ef585c1f1219b14d052a8094eb
AksanAdhi <113028741+AksanAdhi@users.noreply.github.com>
2022-09-08 13:30:13 +0700

    Add files via upload

---
+++ Tugas Pertemuan2_Muhammad Aksan Adhi Pratama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a , b;
+
+  cin >> a >> b;
+
+  cout <<"Jawaban :"<< endl;
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << a * b << endl;
+  cout << a / b << endl;
+  cout << a % b << endl;
+
+
+  return 0;
+}
+  
