e36e5c3f6435e5ea344559f1ec4b9932a7e4e707
AksanAdhi <113028741+AksanAdhi@users.noreply.github.com>
2022-10-19 13:49:59 +0700

    Add files via upload

---
+++ nyoba peergroup.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+//	std::cout<<"Kegiatan:\t Peer Group\n";
+//	std::cout<<"Tempat:\t Online \n";
+
+	cout<<"\"\"\"\"\n";
+	cout<<"\"	\"\n";
+	cout<<"\"	\"\n";
+	cout<<"\"	\"\n";
+	cout<<"\"\"\"\"\n";
+	
+	return 0;	
+}
